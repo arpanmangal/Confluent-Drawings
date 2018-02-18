35f8aeec3fba3dbc1e2cb435a95486f9628a980f
ArpanMangal <mangalarpan@gmail.com>
2018-02-18 16:30:57 +0530

    Basic Express App

---
+++ server.js
@@ -0,0 +1,15 @@
+// An Express Server for running our app
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Servinig Static Content
+app.use(express.static('static'));
+
+// Defining routes
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, console.log("The App is running on PORT: " + PORT +". Visit http://localhost:"+PORT+"/"));
